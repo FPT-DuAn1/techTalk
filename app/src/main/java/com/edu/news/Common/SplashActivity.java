31a968996d5aa84192e904e1ad7c534f2d5f8f2f
KuTin98 <hoangtanphatqn@gmail.com>
2020-04-14 00:46:33 +0700

    up lai splash screen

--- app/src/main/java/com/edu/news/Common/SplashActivity.java
+++ app/src/main/java/com/edu/news/Common/SplashActivity.java
@@ -33,6 +33,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
 
             }
-        }, 3000);
+        }, 6000);
     }
 }
