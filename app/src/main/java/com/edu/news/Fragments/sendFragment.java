9a6d7c49a31a4c0517a9fe8790af9f548c02c6fc
phuocch1999 <phuocch1999@users.noreply.github.com>
2020-04-18 13:15:35 +0700

    gioi thieu va gop y

--- app/src/main/java/com/edu/news/Fragments/sendFragment.java
+++ app/src/main/java/com/edu/news/Fragments/sendFragment.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.edu.news.R;
 
@@ -29,6 +34,10 @@ public class sendFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    public EditText etUsername, etEmail, etGopY;
+    public Button btnGuiGopY;
+    public ProgressBar sendProgessBar;
 
     private OnFragmentInteractionListener mListener;
 
@@ -66,8 +75,44 @@ public class sendFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send2, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_send2, container, false);
+        etUsername = view.findViewById(R.id.etUsername);
+        etEmail = view.findViewById(R.id.etEmail);
+        etGopY = view.findViewById(R.id.etGopY);
+        btnGuiGopY = view.findViewById(R.id.btnGuiGopY);
+        sendProgessBar = view.findViewById(R.id.progressBarGopY);
+        sendProgessBar.setVisibility(View.INVISIBLE);
+
+        btnGuiGopY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                       String username = etUsername.getText().toString();
+                       String email = etEmail.getText().toString();
+                       String gopY = etGopY.getText().toString();
+
+                        if ( username.isEmpty() || email.isEmpty() || gopY.isEmpty()){
+                            Toast.makeText(getContext(), "Xin vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                        }else {
+                            btnGuiGopY.setVisibility(View.INVISIBLE);
+                            sendProgessBar.setVisibility(View.VISIBLE);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    btnGuiGopY.setVisibility(View.VISIBLE);
+                                    sendProgessBar.setVisibility(View.INVISIBLE);
+                                    Toast.makeText(getContext(), "Đã gửi góp ý thành công!", Toast.LENGTH_SHORT).show();
+
+                                    etUsername.setText("");
+                                    etEmail.setText("");
+                                    etGopY.setText("");
+                                }
+                            }, 3000);
+                        }
+                }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
